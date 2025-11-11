8e6fb92f4f6677b6681a1367e33f6274ab393065
varun <varun4socials@gmail.com>
2025-11-11 18:58:43 +0530

    Move constants to db.ts and setup compiler & deployment setting

--- src/components/RatesGraph.tsx
+++ src/components/RatesGraph.tsx
@@ -9,6 +9,8 @@ import {
     ReferenceDot,
 } from "recharts";
 import { TrendingUpIcon, TableIcon } from "lucide-react";
+import data from "../db";
+
 interface RatesGraphProps {
     isSeniorCitizen: boolean;
     onViewChange: () => void;
@@ -19,75 +21,6 @@ export default function RatesGraph({
     isSeniorCitizen,
     onViewChange,
 }: RatesGraphProps) {
-    // Convert tenure periods to plottable data points
-    const data = [
-        {
-            tenure: 0.67,
-            tenureLabel: "7-29 Days",
-            rate: 3.0,
-        },
-        {
-            tenure: 1.25,
-            tenureLabel: "30-45 Days",
-            rate: 3.5,
-        },
-        {
-            tenure: 2.5,
-            tenureLabel: "61-89 Days",
-            rate: 4.5,
-        },
-        {
-            tenure: 7.5,
-            tenureLabel: "6-9 Months",
-            rate: 5.75,
-        },
-        {
-            tenure: 13.5,
-            tenureLabel: "12-15 Months",
-            rate: 6.6,
-        },
-        {
-            tenure: 16.5,
-            tenureLabel: "15-18 Months",
-            rate: 7.1,
-        },
-        {
-            tenure: 19.5,
-            tenureLabel: "18-21 Months",
-            rate: 7.25,
-        },
-        {
-            tenure: 29,
-            tenureLabel: "24-35 Months",
-            rate: 7.0,
-        },
-        {
-            tenure: 32,
-            tenureLabel: "35-35 Months",
-            rate: 7.35,
-        },
-        {
-            tenure: 45,
-            tenureLabel: "36-55 Months",
-            rate: 7.0,
-        },
-        {
-            tenure: 53,
-            tenureLabel: "55-55 Months",
-            rate: 7.4,
-            isBest: true,
-        },
-        {
-            tenure: 58,
-            tenureLabel: "55-60 Months",
-            rate: 7.0,
-        },
-        {
-            tenure: 90,
-            tenureLabel: "60-120 Months",
-            rate: 7.0,
-        },
-    ];
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const CustomTooltip = ({ active, payload }: any) => {
         if (active && payload && payload.length) {
