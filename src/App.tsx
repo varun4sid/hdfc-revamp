297d0346caeaebd69957b60133ec122afe6d30bd
varun <varun4socials@gmail.com>
2025-11-11 19:59:08 +0530

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,24 @@ import "./App.css";
 import CalculateReturns from "./components/CalculateReturns";
 import FDCard from "./components/FDCard";
 import FDComparison from "./components/FDComparison";
+import Footer from "./components/Footer";
 import RatesTable from "./components/RatesTable";
 
 function App() {
     return (
-        <div className="container min-h-screen w-full bg-linear-to-br from-slate-950 via-slate-900 to-slate-950">
-            <div className="column1">
-                <FDCard />
-                <RatesTable />
-                <FDComparison />
+        <>
+            <div className="container min-h-screen w-full bg-linear-to-br from-slate-950 via-slate-900 to-slate-950">
+                <div className="column1">
+                    <FDCard />
+                    <RatesTable />
+                    <FDComparison />
+                </div>
+                <div className="column2">
+                    <CalculateReturns />
+                </div>
             </div>
-            <div className="column2">
-                <CalculateReturns />
-            </div>
-        </div>
+            <Footer />
+        </>
     );
 }
 
